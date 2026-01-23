fe74e8614d8a7421710df96d22c82244463a47ed
Ivan Shibin <thejamesmorra@gmail.com>
2026-01-23 18:08:13 +0300

    implemented classes User.java and Account.java

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,4 +1,55 @@
 package model;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Objects;
+
 public class Account {
+
+    private final long id;
+    private final long userId;
+    private int moneyAmount;
+
+    public Account(long userId, long id, int initBalance) {
+        this.id = id;
+        this.userId = userId;
+        this.moneyAmount = initBalance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public int getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    public void setMoneyAmount(int moneyAmount) {
+        this.moneyAmount = moneyAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Account account)) return false;
+        return id == account.id && userId == account.userId && moneyAmount == account.moneyAmount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, moneyAmount);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", moneyAmount=" + moneyAmount +
+                '}';
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,56 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class User {
+
+    private final long id;
+    private final List<Account> accountList;
+    private String login;
+
+    public User(long id, String login) {
+        this.id = id;
+        this.login = login;
+        this.accountList = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public List<Account> getAccountList() {
+        return accountList;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return id == user.id && Objects.equals(accountList, user.accountList) && Objects.equals(login, user.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, accountList, login);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", accountList=" + accountList +
+                '}';
+    }
 }
